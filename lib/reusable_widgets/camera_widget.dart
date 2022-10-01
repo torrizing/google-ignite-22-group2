e9d84116c905903a589730f67660a76055a6b25d
joshuadavidang <chunxiong_@outlook.sg>
2022-10-01 11:57:26 +0800

    added camera widget dart file

---
+++ lib/reusable_widgets/camera_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CameraWidget extends StatefulWidget {
+  const CameraWidget({super.key});
+
+  @override
+  State<CameraWidget> createState() => _CameraWidgetState();
+}
+
+class _CameraWidgetState extends State<CameraWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
