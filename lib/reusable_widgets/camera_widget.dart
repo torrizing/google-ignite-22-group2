6c6c89f0828c7e3317b622d7cc5bf147c8cac73d
joshuadavidang <chunxiong_@outlook.sg>
2022-10-02 13:44:41 +0800

    camera widget

--- lib/reusable_widgets/camera_widget.dart
+++ lib/reusable_widgets/camera_widget.dart
@@ -1,65 +1,63 @@
-// test
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
 
-// import 'package:camera/camera.dart';
-// import 'package:flutter/material.dart';
+late List<CameraDescription> _cameras;
 
-// late List<CameraDescription> _cameras;
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
-// Future<void> main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
+  _cameras = await availableCameras();
+  runApp(const CameraApp());
+}
 
-//   _cameras = await availableCameras();
-//   runApp(const CameraApp());
-// }
+/// CameraApp is the Main Application.
+class CameraApp extends StatefulWidget {
+  /// Default Constructor
+  const CameraApp({Key? key}) : super(key: key);
 
-// /// CameraApp is the Main Application.
-// class CameraApp extends StatefulWidget {
-//   /// Default Constructor
-//   const CameraApp({Key? key}) : super(key: key);
+  @override
+  State<CameraApp> createState() => _CameraAppState();
+}
 
-//   @override
-//   State<CameraApp> createState() => _CameraAppState();
-// }
+class _CameraAppState extends State<CameraApp> {
+  late CameraController controller;
 
-// class _CameraAppState extends State<CameraApp> {
-//   late CameraController controller;
+  @override
+  void initState() {
+    super.initState();
+    controller = CameraController(_cameras[0], ResolutionPreset.max);
+    controller.initialize().then((_) {
+      if (!mounted) {
+        return;
+      }
+      setState(() {});
+    }).catchError((Object e) {
+      if (e is CameraException) {
+        switch (e.code) {
+          case 'CameraAccessDenied':
+            print('User denied camera access.');
+            break;
+          default:
+            print('Handle other errors.');
+            break;
+        }
+      }
+    });
+  }
 
-//   @override
-//   void initState() {
-//     super.initState();
-//     controller = CameraController(_cameras[0], ResolutionPreset.max);
-//     controller.initialize().then((_) {
-//       if (!mounted) {
-//         return;
-//       }
-//       setState(() {});
-//     }).catchError((Object e) {
-//       if (e is CameraException) {
-//         switch (e.code) {
-//           case 'CameraAccessDenied':
-//             print('User denied camera access.');
-//             break;
-//           default:
-//             print('Handle other errors.');
-//             break;
-//         }
-//       }
-//     });
-//   }
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
 
-//   @override
-//   void dispose() {
-//     controller.dispose();
-//     super.dispose();
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     if (!controller.value.isInitialized) {
-//       return Container();
-//     }
-//     return MaterialApp(
-//       home: CameraPreview(controller),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    if (!controller.value.isInitialized) {
+      return Container();
+    }
+    return MaterialApp(
+      home: CameraPreview(controller),
+    );
+  }
+}
