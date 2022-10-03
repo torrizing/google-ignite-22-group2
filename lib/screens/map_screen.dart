292f4a39907ec785b9e44c75405929d5eab79b3d
joshuadavidang <chunxiong_@outlook.sg>
2022-10-03 09:28:14 +0800

    implemented latlng

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -1,3 +1,57 @@
+// import 'package:flutter/material.dart';
+// import 'package:google_maps_flutter/google_maps_flutter.dart';
+// import '../src/locations.dart' as locations;
+
+// class MapScreen extends StatefulWidget {
+//   const MapScreen({super.key});
+
+//   @override
+//   State<MapScreen> createState() => _MapScreenState();
+// }
+
+// class _MapScreenState extends State<MapScreen> {
+//   final Map<String, Marker> _markers = {};
+//   Future<void> _onMapCreated(GoogleMapController controller) async {
+//     final googleOffices = await locations.getGoogleOffices();
+//     setState(() {
+//       _markers.clear();
+//       for (final office in googleOffices.offices) {
+//         final marker = Marker(
+//           markerId: MarkerId(office.name),
+//           position: LatLng(office.lat, office.lng),
+//           infoWindow: InfoWindow(
+//             title: office.name,
+//             snippet: office.address,
+//           ),
+//         );
+//         _markers[office.name] = marker;
+//       }
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         backgroundColor: Colors.white,
+//         appBar: AppBar(
+//             iconTheme: const IconThemeData(color: Colors.black),
+//             title: const Text(
+//               'Map',
+//               style: TextStyle(color: Colors.black),
+//             ),
+//             elevation: 0,
+//             backgroundColor: Colors.white),
+//         body: GoogleMap(
+//           onMapCreated: _onMapCreated,
+//           initialCameraPosition: const CameraPosition(
+//             target: LatLng(0, 0),
+//             zoom: 2,
+//           ),
+//           markers: _markers.values.toSet(),
+//         ));
+//   }
+// }
+
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import '../src/locations.dart' as locations;
@@ -9,6 +63,8 @@ class MapScreen extends StatefulWidget {
   State<MapScreen> createState() => _MapScreenState();
 }
 
+// test
+
 class _MapScreenState extends State<MapScreen> {
   final Map<String, Marker> _markers = {};
   Future<void> _onMapCreated(GoogleMapController controller) async {
@@ -18,7 +74,7 @@ class _MapScreenState extends State<MapScreen> {
       for (final office in googleOffices.offices) {
         final marker = Marker(
           markerId: MarkerId(office.name),
-          position: LatLng(office.lat, office.lng),
+          position: const LatLng(1.415665, 103.839016),
           infoWindow: InfoWindow(
             title: office.name,
             snippet: office.address,
